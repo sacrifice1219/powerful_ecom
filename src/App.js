9cd0cd573f00a29705b56bdd28ccfa444027dbab
lucky favour <luckyfavour1219@gmail.com>
2024-07-22 16:40:24 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Nav from './Navigation/Nav';
+import Products from './Products/Products';
+import Recommended from './Recommended/Recommended';
+import Sidebar from './Sidebar/Sidebar';
+
+//database
+import products from './db/data';
+import Category from './Sidebar/Category/Category';
+import Card from './components/Card';
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+
+  //input filter
+  const [query, setQuery] = useState('');
+
+  const handleInputChange = event => {
+    setQuery(event.target.value);
+  }
+
+  const filteredItems = products.filter((product) => product.title.toLocaleLowerCase().indexOf(query.toLocaleLowerCase() !== -1)
+  );
+
+  //radio filters
+  const handleChange = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+  //buttons filters
+  const handleClick = event => {
+    setSelectedCategory(event.target.value)
+  }
+
+  //function
+  const filteredData = (products, selected, query) => {
+    let filteredProducts = products;
+
+    //filtering input items
+    if (query) {
+      filteredProducts = filteredItems
+    }
+
+
+    //selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
+  const result = filteredData(products, selectedCategory, query);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < >
+       <Sidebar handleChange={handleChange} />
+      <Nav query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick} />
+      <Products result={result} />
+    </>
   );
 }
 
---
+++ src/components/Input.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Input = ({handleChange, value, title, name, color}) => {
+  return (
+    <div>
+       
+       <label className="sidebar-label-container color-title">
+        <input onChange={handleChange} type="radio" value={value} name={name} />
+        <span className="checkmark" style={{backgroundColor: color}}></span>{title}
+      </label>
+    </div>
+  )
+}
+
+export default Input
